c3bd57a7ca529e3d085a9d81f989c4cfb5d855bd
jao-87 <james.ostrowski.ii@gmail.com>
2020-06-27 10:32:11 -0700

    Add interactions

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$('#workWash').on("click", function(){
+    window.location.href = "https://docs.google.com/presentation/d/1L9ZPCtjIvoQYujcwI29U8J67t_na4neyZYPnjZOy1ws/edit#slide=id.p";
+});
+
+$('#workCurEater').on("click", function(){
+    window.location.href = "https://docs.google.com/presentation/d/1-g4iegEkyMrFYs38vlxhgp41_5UwltVSY3nIHXGemjg/edit";
+});
